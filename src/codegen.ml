5ab0f89501f1a8ea0bd155327037441d5ad9cf80
Mateusz Lewko <mateusz.lewko@gmail.com>
2018-01-08 18:44:33 +0100

    Added tasks.

--- src/codegen.ml
+++ src/codegen.ml
@@ -4,4 +4,17 @@ open Core
 
 type environment = { m : int }
 
+let literal ctx = 
+  function
+  | Int i  -> const_int (i32_type ctx) i
+  | Bool b -> const_int (i1_type ctx)  (BatBool.to_int b)
+  | other  -> show_literal other |> sprintf "Unsupported literal: %s"
+              |> failwith
+
+let letexp ctx (name, args, fst_line, snd_line) = 
+  let local = create_context () in 
+  let builder = builder local in 
+  
+  ()
+
 let main () = printf "<main>\n"
