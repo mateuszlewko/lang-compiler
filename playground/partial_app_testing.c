0c743bbeabca2c0b22a7febe626081383e4d0be0
Mateusz Lewko <mateusz.lewko@gmail.com>
2018-03-07 01:48:49 +0100

    Partial application with too many args almost done (in c).

--- playground/partial_app_testing.c
+++ playground/partial_app_testing.c
@@ -6,88 +6,64 @@
 typedef unsigned char uchar;
 
 struct thunk {
-    void* (*fn)(char**);
+    void* (*fn)(char*);
     char* args;
     uchar left_arity;
     size_t curr_bytes;
-};
-
-struct arg2 {
-    char x1;
-    float x2;
-};
-
-struct add_args {
-    int x1;
-    int* x2;
+    size_t needed_bytes;
 };
 
 /*
-
-f0 : int -> int -> int 
-x  : int 
-
-f1 : int -> int = f0 x 
-
-f2_temp : int -> int 
-
-f2 x : int -> int -> int = f1
-
-f3 : int -> int = f2 x
-
 let getAdder x = 
     let adder x y z = 
         let innerAdder x y z w q = x + y + z + w + q
         in innerAdder x y z 
     in adder x 
-
-let getAdder x : int -> (int -> int -> int -> int -> int) = 
-    let adder x y z : int -> int -> int -> (int -> int -> int) = 
-        let innerAdder x y z w q : int -> int -> int -> int -> int -> int == 
-            x + y + z + w + q
-        innerAdder x y z 
-    adder x 
 */
+
+int innerAdder(int x, int y, int z, int w, int q) {
+    return x + y + z + w + q;
+}
 
 int pre_innerAdder(char* data) {
     int* args = (int*)data;
     return innerAdder(args[0], args[1], args[2], args[3], args[4]);
 }
 
-int innerAdder(int x, int y, int z, int w, int q) {
-    return x + y + z + w + q;
-}
-
-struct thunk adder(int x, int y, int z) {
-    struct thunk th;
+struct thunk* adder(int x, int y, int z) {
+    struct thunk* th = malloc(sizeof(struct thunk));
     
-    th.fn = &pre_innerAdder;
-    th.left_arity = 2;
-    th.args = malloc(sizeof(int) * 3);
+    th->fn = &pre_innerAdder;
+    th->left_arity = 2;
+    th->args = malloc(sizeof(int) * 3);
+    th->curr_bytes = sizeof(int) * 3;
+    th->needed_bytes = sizeof(int) * 5;
 
     int data[] = {x, y, z};
 
-    memcpy(th.args, data, sizeof(int) * 3);
-    return th;;
+    memcpy(th->args, (void*)data, sizeof(int) * 3);
+    return th;
 }
 
-struct thunk pre_adder(char* data) {
+struct thunk* pre_adder(char* data) {
     int* args = (int*)data;
     return adder(args[0], args[1], args[2]);
 }
 
-struct thunk getAdder(int x) {
-    struct thunk th;
-    th.fn = &pre_adder;
-    th.left_arity = 2;
-    th.args = malloc(sizeof(int));
-    th.curr_bytes = sizeof(int);
-    memcpy(th.args, &x, sizeof(int));
+struct thunk* getAdder(int x) {
+    struct thunk* th = malloc(sizeof(struct thunk));
+    
+    th->fn = &pre_adder;
+    th->left_arity = 2;
+    th->args = malloc(sizeof(int));
+    th->curr_bytes = sizeof(int);
+    th->needed_bytes = sizeof(int) * 3;
 
+    memcpy(th->args, &x, sizeof(int));
     return th;
 }
 
-struct thunk pre_getAdder(char* data) {
+struct thunk* pre_getAdder(char* data) {
     return getAdder(((int*)data)[0]);
 }
 
@@ -96,18 +72,29 @@ char* generic_apply(struct thunk* th, uchar cnt, char* args, size_t args_size) {
 
     // copy all required bytes to one array
     memcpy(final_args, th->args, th->curr_bytes);
-    memcpy(final_args + th->curr_bytes, args, args_size);
+    size_t diff_bytes = th->needed_bytes - th->curr_bytes;
+    size_t used_bytes = args_size < diff_bytes ? args_size : diff_bytes;
+    memcpy(final_args + th->curr_bytes, args, used_bytes);
 
-    if (th->left_arity == cnt) {
+    if (th->left_arity <= cnt) {
         // pass thunk with all args (as byte array) to th->fn
-        return th->fn(final_args);
+        if (used_bytes == args_size) 
+            return (char*)(th->fn(final_args));
+        else {
+            struct thunk* mid_th = (struct thunk*)th->fn(final_args);
+            return generic_apply(mid_th, 
+                                 cnt - th->left_arity,
+                                 args + used_bytes, args_size - used_bytes);
+        }
     }
     else {
         struct thunk* res = malloc(sizeof(struct thunk));
+
         res->left_arity = th->left_arity - cnt;
         res->args = final_args;
         res->fn = th->fn;
         res->curr_bytes = th->curr_bytes + args_size;
+        res->needed_bytes = th->needed_bytes;
 
         return (char*)res;
     }
@@ -115,77 +102,20 @@ char* generic_apply(struct thunk* th, uchar cnt, char* args, size_t args_size) {
 
 struct thunk g;
 
-int rrrr = 1;
-
 void test() {
-    struct thunk th = getAdder(1<<20);
-    th;
+    struct thunk* th = getAdder(1<<20);
+    int next_args[] = {2, 15, 19};
+    struct thunk* res1 = (struct thunk*)generic_apply(th, 3, (char*)&next_args, sizeof(int) * 3);
 
-    int next_arg = 2;
-    char* res1 = generic_apply(&th, 1, (char*)&next_arg, sizeof(int));
-    res1;
-
-    rrrr ^= (int)&th;
-    rrrr ^= (int)res1;
-
-    struct thunk th2;
-    memcpy(&th2, res1, sizeof(th2));
-    g = th2;
-    rrrr ^= (int)&g;
+    for (int i = 0; i < 4; i++) {
+        printf("%d\n", ((int*)res1->args)[i]);
+    }
 }
-
-int x;
 
 int fast_add(int a, int b, int c, int d) {
     return a + b + c + d;
 }
 
-// void* apply2 (void* (*fn)(void*, void*), void** args) {
-//     return fn(args[0], args[1]);
-// }
-
-// void* some_apply(void* some, void** args) {
-//     void* (*fn)(void*, void*) = some;
-//     apply2(fn, args);
-// }
-
-char* retArr() {
-    char tmp[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    char* res = malloc(12);
-    memcpy(res, tmp, 12);
-    return res;
-}
-
 int main() {
     test();
-
-    // char* res = retArr();
-
-    // for (int i = 0; i < 12; i++) {
-    //     printf("%d ", (int)res[i]);
-    // }
-    // puts("");
-
-    // int n = 1000000000;
-    
-    // clock_t t;
-    // t = clock();
-    
-    // for (int i = 0; i < n; i++)
-    //     x ^= fast_add(i, i - 100, i * i, i * 100 + (i - 100 * i));
-    
-    // t = clock() - t;
-    // double time_taken = ((double)t)/CLOCKS_PER_SEC;
-    // printf("Elapsed time: %.3lf\n", time_taken);
-
-    // t = clock();
-    
-    // for (int i = 0; i < n; i++)
-    //     test ();
-    // printf("%d", rrrr);
-
-    // t = clock() - t;
-    // time_taken = ((double)t)/CLOCKS_PER_SEC;
-    // printf("Elapsed time: %.3lf\n", time_taken);
-
 }
